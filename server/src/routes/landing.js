5f055c2e9446424cdb82104e10b16dadb7f979eb
tope <temitope.emmanuel63@gmail.com>
2017-09-09 23:44:05 +0100

    Remove irrelevant files

--- server/src/routes/landing.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-//  show index page
-router.get('/api', (req, res) => {
-  res.send({ message: '<h1>hello landing page<h1>' });
-});
-export default router;
