e11fa1dfe0c1d0219ba5614149df63ed13795d2a
Temitope Emmanuel <temitope.emmanuel63@gmail.com>
2017-10-05 14:29:15 +0100

    Remove postbuild command from package.json

--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -10,8 +10,10 @@ router.post('/api/admin/signup', adminControllers.create);
 //  signin admin
 router.post('/api/admin/signin', adminControllers.findAdmin);
 router.get('/api/admins', adminControllers.findAdmins);
+// Get all users exceeding deadline
 router.get('/api/admins/exceed-deadlines', authorize.checkAuthentication, authorize.authorizeAdmin,
   borrowBookControllers.exceedDeadline);
+//Charge user
 router.put('/api/admins/charge-user/:userId/:bookId', authorize.checkAuthentication,
   authorize.authorizeAdmin, borrowBookControllers.chargeUser);
 
