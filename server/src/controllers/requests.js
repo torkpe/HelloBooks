378762ff7e47a7b91dedf03e9bfd90d3d64d5a06
Temitope Emmanuel <temitope.emmanuel63@gmail.com>
2017-11-13 12:36:08 +0100

    Remove server/ folder

--- server/src/controllers/requests.js
+++
@@ -1,5 +0,0 @@
-import model from '../models';
-
-const Book = model.Book;
-const borrowBook = model.BorrowBook;
-const requests = model.Requests;
--- server/src/routes/landing.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-//  show index page
-router.get('/', (req, res) => {
-  res.send({ message: '<h1>hello landing page<h1>' });
-});
-export default router;
